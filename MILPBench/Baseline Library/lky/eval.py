a1d4f143ccb40352d765abb7f9040c5d5c439e52
as_lky <luoky22@mails.tsinghua.edu.cn>
2025-04-12 03:39:52 +0800

    update eval

--- MILPBench/Baseline Library/lky/eval.py
+++ MILPBench/Baseline Library/lky/eval.py
@@ -7,6 +7,10 @@ import re
 import subprocess
 import json
 import shutil
+import math
+from scipy.interpolate import PchipInterpolator
+from scipy.optimize import root_scalar
+
 
 parser = argparse.ArgumentParser(description="receive evaluate instruction")
 parser.add_argument("--taskname", required=True, choices=["IP", "IS", "WA", "CA"], help="taskname")
@@ -34,9 +38,9 @@ def work_gurobi(instance):
         data = json.load(f)
     
     if data['type'] == -1: # 最大化
-        return data['obj'] * (1 + data['gap'] / 100)
+        return data['obj'] * (1 + data['gap'] / 100), -1
     else:
-        return data['obj'] * (1 - data['gap'] / 100)
+        return data['obj'] * (1 - data['gap'] / 100), 1
     
 instance_path = args.instance_path
 train_data_dir = args.train_data_dir
@@ -44,27 +48,95 @@ train_data_dir = args.train_data_dir
 grlis = ["bi", "bir", "default"]
 prelis = ["gcn", "gurobi", "scip", "cplex"]
 modlis = ["sr", "nr", "np", "default"]
-sealis = ["gurobi", "scip", "LIH", "MIH", "LNS", "NALNS", "ACP"]
+sealis = ["gurobi", "LIH", "MIH", "LNS", "NALNS", "ACP"]
 instancelis = [os.path.join(instance_path, file) for file in os.listdir(instance_path) if c(file)] # 10 instances
 
 score_dic = {}
 
-def calc(now_score):
+def calc_api(lis):
+    # lis 为一个list 其元素为一个list，对应一个点
     if os.path.exists('calc/hbda/build/nonincremental/tmp.txt'):
         os.remove('calc/hbda/build/nonincremental/tmp.txt')
     SUM = 0
     with open('calc/hbda/build/nonincremental/tmp.txt', 'w') as f:
-        for _ in now_score.keys():
-            for we in range(3):
-                for __ in _:
-                    f.write(f"{__} ")
+        for i in lis:
+            for j in i:
+                w = round(j, 8)
+                f.write(f"{w} ")
             f.write("\n")
-        subprocess.run(['calc/hbda/build/nonincremental/nonincremental', '-O', 'calc/hbda/build/nonincremental/tmp.txt'])
-        
+    subprocess.run(['./calc/hbda/build/nonincremental/nonincremental', '-O', 'calc/hbda/build/nonincremental/tmp.txt'])    
     return 0
 
+def gapstart_c(time_list, val_list, lobj): # 初始解gap
+    val = val_list[0]
+    gap = abs(val - lobj) / val 
+    return min(gap / 1000, 1) # 越小越好
+
+def gapend_c(time_list, val_list, lobj): # 最终gap
+    val = val_list[-1]
+    gap = abs(val - lobj) / val 
+    return min(gap / 10, 1) # 越小越好
+
+def ir_c(time_list, val_list, lobj): # 改进比率
+    ir = abs(val_list[-1] - val_list[0]) / val_list[0]
+    if ir < 0.01:
+        ir = 0.01
+    if ir > 100000:
+        ir = 100000
+    # 差距可能很大！
+    a = math.log(ir) # -2 到 5
+    a = 1 - (a + 2) / 7
+    return a # 越小越好
+
+def nr_c(time_list, val_list, lobj): # 求解的有效率
+    num = 0
+    for _ in val_list:
+        if abs(_ - lobj) / lobj < 0.001:
+            num += 1
+    nr = num / len(val_list) 
+    return 1 - nr # 越小越好
+
+def stime_c(time_list, val_list, lobj): # 求解的预估收敛时间
+    pchip = PchipInterpolator(time_list, val_list)    
+    def func(x):
+        return pchip(x) - lobj
+    stime = root_scalar(func, bracket=[0.1, 1000000])
+    if stime.converged:
+        stime = math.log(stime.root) # -1 到 6
+        return (stime + 1) / 7 # 越小越好
+    else:
+        return 1 # 越小越好
+
+#TODO:check求解的稳定性
+
+def calc(data, lobj):
+    # 最终gap
+    # TODO: 做数学推导
+    result_list = data['result_list']
+    time_list = [_[0] for _ in result_list]
+    val_list = [_[1] for _ in result_list]
+    
+    ll1 = [gapstart_c(time_list, val_list, lobj), 
+          gapend_c(time_list, val_list, lobj), 
+          ir_c(time_list, val_list, lobj),
+          nr_c(time_list, val_list, lobj),
+          stime_c(time_list, val_list, lobj)]
+    ll2 = [0.1, 0.4, 0.2, 0.1, 0.2]
+    for i in range(len(ll1)):
+        ll1[i] *= ll2[i]
+        ll1[i] /= 0.4  
+    return ll1
+
+    # TODO:加入更多指标
+    # 几个收敛指标?
+    # 稳定性指标？
+    # 加入大模型的
+    ...
+    
+scores = {}
+
 for instance in instancelis:
-    lobj = work_gurobi(instance)
+    lobj, type_ = work_gurobi(instance)
     now_score = {}
     for gr in grlis:
         for pre in prelis:
@@ -92,8 +164,18 @@ for instance in instancelis:
                     des = f'./logs/work/{args.taskname}/{we}/{tmp}_result.txt'
                     with open(des, 'r') as f:
                         data = json.load(f)
-                    print(data)
-                    now_score[we] = [abs(data['obj'] - lobj) / data['obj'] * 100]
-    #calc(now_score)        
+                    now_score[we] = calc(data, lobj)
     
+    LIS = []
+    for key, value in now_score.items():
+        LIS.append(value)
+    SUM = calc_api(LIS)
+    for key, value in now_score.items():
+        LIS.remove(value)
+        sum = calc_api(LIS)
+        scores[we] = scores.get(we, 0) + SUM - sum
+        LIS.append(value)
+
+for key, value in scores.items():
+    print(key, value)    
     
